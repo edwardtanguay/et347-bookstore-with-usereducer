f02f98fd5dfcabc77e92d5d789e6f4080d0516f5
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-28 11:44:42 +0100

    change title works

--- src/pages/PageDemo.tsx
+++ src/pages/PageDemo.tsx
@@ -2,20 +2,27 @@ import { useReducer } from "react";
 
 interface IState {
 	count: number;
+	title: string;
 	error: string;
 }
 
 const initialState: IState = {
 	count: 0,
+	title: "The useReducer/reducer Demo",
 	error: "",
 };
 
-interface IAction {
+interface INumberAction {
 	type: "increment" | "decrement";
 	payload: number;
 }
 
-const reducer = (state: IState, action: IAction) => {
+interface IStringAction {
+	type: "setTitle";
+	payload: string;
+}
+
+const reducer = (state: IState, action: INumberAction | IStringAction) => {
 	const _state = structuredClone(state);
 	switch (action.type) {
 		case "increment":
@@ -24,8 +31,11 @@ const reducer = (state: IState, action: IAction) => {
 		case "decrement":
 			_state.count -= action.payload;
 			break;
+		case "setTitle":
+			_state.title = action.payload;
+			break;
 		default:
-			_state.error = "bad action type: " + action.type;
+			_state.error = "bad action type"; // TODO: figure out how to display action.type, it got a typescript error
 	}
 	return _state;
 };
@@ -35,12 +45,23 @@ export const PageDemo = () => {
 
 	return (
 		<>
-		<h2 className="text-2xl mb-3">Demo of useReducer</h2>
+			<h2 className="text-2xl mb-3">{state.title}</h2>
 			<div className="flex gap-3">
-				<button onClick={() => dispatch({type: 'decrement', payload: 1})}>-</button>
-				<button onClick={() => dispatch({type: 'increment', payload: 1})}>+</button>
+				<button
+					onClick={() => dispatch({ type: "decrement", payload: 1 })}
+				>
+					-
+				</button>
+				<button
+					onClick={() => dispatch({ type: "increment", payload: 1 })}
+				>
+					+
+				</button>
 				<p className="text-xl mt-1">count = {state.count}</p>
-		</div>
+			</div>
+			<div>
+				Change title: <input className="w-80" value={state.title} onChange={(e) => dispatch({type: 'setTitle', payload: e.target.value})} type="text"/>
+			</div>
 		</>
-	)
+	);
 };
