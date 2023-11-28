3fce8e292343fd47b949116f63e22e0b31c1924a
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-28 11:23:19 +0100

    minimal reducer in

--- src/pages/PageDemo.tsx
+++ src/pages/PageDemo.tsx
@@ -1,5 +1,37 @@
-export const PageDemo = () => {
-	return (
-		<p>this is the demo page</p>
-	)
+import { useReducer } from "react";
+
+interface IState {
+	count: number;
+	error: string;
 }
+
+const initialState: IState = {
+	count: 999,
+	error: "",
+};
+
+interface IAction {
+	type: "increment" | "decrement";
+	payload: number;
+}
+
+const reducer = (state: IState, action: IAction) => {
+	const _state = structuredClone(state);
+	switch (action.type) {
+		case "increment":
+			_state.count += action.payload;
+			break;
+		case "decrement":
+			_state.count -= action.payload;
+			break;
+		default:
+			_state.error = "bad action type: " + action.type;
+	}
+	return _state;
+};
+
+export const PageDemo = () => {
+	const [state] = useReducer(reducer, initialState);
+
+	return <p>this is the demo page, count = {state.count}</p>;
+};
